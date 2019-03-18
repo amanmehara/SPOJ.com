04e6a1ff46989bd36b3acf4a37b9a19c2aa2b6fa
Aman Mehara <amanmehara@hotmail.com>
2019-03-18 14:25:24 +0530

    PRIME1

--- PRIME1.cpp
+++ PRIME1.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 std::vector<std::int64_t> sieve_of_eratosthenes(std::int64_t number) {
@@ -22,24 +24,29 @@ std::vector<std::int64_t> sieve_of_eratosthenes(std::int64_t number) {
     return primes;
 }
 
-std::vector<std::int64_t> primes(std::int64_t begin, std::int64_t end) {
-    std::vector<std::int64_t> primes;
-    if (begin <= std::sqrt(end) || end <= 100000) {
-        primes = sieve_of_eratosthenes(end);
-    } else {
-        primes = sieve_of_eratosthenes(std::sqrt(end));
-        std::vector<bool> is_prime(end - begin + 1, true);
-        for (size_t i = 0; i < primes.size(); i++) {
-            std::int64_t mod = begin % primes[i];
-            for (size_t j = (mod == 0) ? begin : begin - mod + primes[i]; j <= end; j += primes[i]) {
-                is_prime[j - begin] = false;
-            }
+std::vector<std::int64_t> primes(std::int64_t begin, std::int64_t end, std::vector<std::int64_t> primes) {
+    std::vector<bool> is_prime(end - begin + 1, true);    
+    if(begin == 1) {
+        is_prime[0] = false;
+    }
+    for (size_t i = 0; i < primes.size(); i++) {
+        if(primes[i] * primes[i] > end) {
+            break;
         }
-        primes.clear();
-        for (size_t i = 0; i < is_prime.size(); i++) {
-            if (is_prime[i]) {
-                primes.push_back(i + begin);
-            }
+        std::int64_t mod = begin % primes[i];
+        size_t j = (mod == 0) ? begin : begin - mod + primes[i];
+        if (j <= primes[i]) {
+            j += primes[i];
+        }
+        while (j <= end) {
+            is_prime[j - begin] = false;
+            j += primes[i];
+        }
+    }
+    primes.clear();
+    for (size_t i = 0; i < is_prime.size(); i++) {
+        if (is_prime[i]) {
+            primes.push_back(i + begin);
         }
     }
     return primes;
@@ -48,10 +55,17 @@ std::vector<std::int64_t> primes(std::int64_t begin, std::int64_t end) {
 int main() {
     int number;
     std::cin >> number;
+    std::vector<std::pair<std::int64_t, std::int64_t>> inputs;
+    std::int64_t max = 1;
     while (number--) {
         std::int64_t begin, end;
         std::cin >> begin >> end;
-        for (std::int64_t prime : primes(begin, end)) {
+        inputs.push_back(std::make_pair(begin, end));
+        max = std::max(max, end);
+    }
+    auto primes_ = sieve_of_eratosthenes(std::sqrt(max));
+    for(auto range: inputs) {
+        for (std::int64_t prime : primes(range.first, range.second, primes_)) {
             std::cout << prime << std::endl;
         }
         std::cout << std::endl;
